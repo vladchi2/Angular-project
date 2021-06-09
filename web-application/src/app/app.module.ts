0d0560a447173d32e9c5007ece2346d1774a40ae
Vlad Cojocariu <408484@student.fontys.nl>
2021-06-09 19:42:36 +0300

    up

---
+++ web-application/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { EmployeesComponent } from './employees/employees.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DepartmentsComponent,
+    EmployeesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
